de270bf36f907002fae902a565797b618ece58be
elifKurtay <elif.kurtay@oracle.com>
2024-10-25 17:39:26 +0200

    update tests

--- sourcegen-generator-java/src/test/java/io/micronaut/sourcegen/javapoet/write/InnerTypesTest.java
+++ sourcegen-generator-java/src/test/java/io/micronaut/sourcegen/javapoet/write/InnerTypesTest.java
@@ -1,9 +1,5 @@
 package io.micronaut.sourcegen.javapoet.write;
 
-import io.micronaut.inject.processing.ProcessingException;
-import io.micronaut.inject.visitor.VisitorContext;
-import io.micronaut.sourcegen.generator.SourceGenerator;
-import io.micronaut.sourcegen.generator.SourceGenerators;
 import io.micronaut.sourcegen.model.ClassDef;
 import io.micronaut.sourcegen.model.EnumDef;
 import io.micronaut.sourcegen.model.InterfaceDef;
@@ -13,242 +9,284 @@ import io.micronaut.sourcegen.model.RecordDef;
 import io.micronaut.sourcegen.model.TypeDef;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileWriter;
+import javax.lang.model.element.Modifier;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-public class InnerTypesTest {
-
-    private static void assertAndClean(File classBuilder, String expectedString) throws IOException {
-        File outputFile = classBuilder;
-        assertTrue(outputFile.exists());
-        String actual = compareFileContentWithString(outputFile, expectedString);
-        assertEquals(expectedString, actual);
-        outputFile.delete();
-    }
-
+public class InnerTypesTest extends AbstractWriteTest {
+    /** -----------------------------------------------------------
+     * INNER TYPES INSIDE A CLASS
+     * -----------------------------------------------------------
+     */
     @Test
     public void enumInClass() throws IOException {
-        String expectedString = "class StatusClass {\n" +
-            "  enum Status {\n" +
-            "\n" +
-            "    SINGLE,\n" +
-            "    MARRIED\n" +
-            "  }\n" +
-            "}\n";
+        String expectedString = """
+            package test;
+
+            public class StatusClass {
+              enum Status {
+
+                SINGLE,
+                MARRIED
+              }
+            }""";
         EnumDef.EnumDefBuilder enumBuilder = EnumDef.builder("Status");
         enumBuilder.addEnumConstant("SINGLE").addEnumConstant("MARRIED");
         EnumDef enumDef = enumBuilder.build();
 
         ClassDef.ClassDefBuilder classBuilder = getClassDefBuilderWith(enumDef);
-        assertAndClean(generateClass(classBuilder.build()), expectedString);
+        String actual = writeClass(classBuilder.build(), "class");
+        assertEquals(expectedString.strip(), actual);
     }
 
     @Test
     public void recordInClass() throws IOException {
-        String expectedString = "class ExampleRecordClass {\n" +
-            "  record ExampleRecord(\n" +
-            "      int id\n" +
-            "  ) {\n" +
-            "  }\n" +
-            "}\n";
+        String expectedString = """
+            package test;
+
+            public class ExampleRecordClass {
+              record ExampleRecord(
+                  int id
+              ) {
+              }
+            }""";
         RecordDef.RecordDefBuilder recordBuilder = RecordDef.builder("ExampleRecord");
         PropertyDef.PropertyDefBuilder propertyBuilder = PropertyDef.builder("id").ofType(TypeDef.Primitive.INT);
         recordBuilder.addProperty(propertyBuilder.build());
 
         ClassDef.ClassDefBuilder classBuilder = getClassDefBuilderWith(recordBuilder.build());
-        assertAndClean(generateClass(classBuilder.build()), expectedString);
+        String actual = writeClass(classBuilder.build(), "class");
+        assertEquals(expectedString.strip(), actual);
     }
 
     @Test
     public void classInClass() throws IOException {
-        String expectedString = "class InnerClass {\n" +
-            "  class Inner {\n" +
-            "  }\n" +
-            "}\n";
+        String expectedString = """
+            package test;
+
+            public class InnerClass {
+              class Inner {
+              }
+            }""";
         ClassDef.ClassDefBuilder innerClassBuilder = ClassDef.builder("Inner");
 
         ClassDef.ClassDefBuilder classBuilder = getClassDefBuilderWith(innerClassBuilder.build());
-        assertAndClean(generateClass(classBuilder.build()), expectedString);
+        String actual = writeClass(classBuilder.build(),"class");
+        assertEquals(expectedString.strip(), actual);
     }
 
     @Test
     public void interfaceInClass() throws IOException {
-        String expectedString = "class InterfaceClass {\n" +
-            "  interface Interface {\n" +
-            "  }\n" +
-            "}\n";
+        String expectedString = """
+            package test;
+
+            public class InterfaceClass {
+              interface Interface {
+              }
+            }""";
         InterfaceDef.InterfaceDefBuilder interfaceBuilder = InterfaceDef.builder("Interface");
 
         ClassDef.ClassDefBuilder classBuilder = getClassDefBuilderWith(interfaceBuilder.build());
-        assertAndClean(generateClass(classBuilder.build()), expectedString);
+        String actual = writeClass(classBuilder.build(),"class");
+        assertEquals(expectedString.strip(), actual);
     }
 
+    /** -----------------------------------------------------------
+     * INNER TYPES INSIDE A RECORD
+     * -----------------------------------------------------------
+     */
     @Test
     public void enumInRecord() throws IOException {
-        String expectedString = "record StatusRecord() {\n" +
-            "  enum Status {\n" +
-            "\n" +
-            "    SINGLE,\n" +
-            "    MARRIED\n" +
-            "  }\n" +
-            "}\n";
+        String expectedString = """
+            package test;
+
+            public record StatusRecord() {
+              enum Status {
+
+                SINGLE,
+                MARRIED
+              }
+            }""";
         EnumDef.EnumDefBuilder enumBuilder = EnumDef.builder("Status");
         enumBuilder.addEnumConstant("SINGLE").addEnumConstant("MARRIED");
         EnumDef enumDef = enumBuilder.build();
 
         RecordDef.RecordDefBuilder classBuilder = getRecordDefBuilderWith(enumDef);
-        assertAndClean(generateClass(classBuilder.build()), expectedString);
+        String actual = writeClass(classBuilder.build(), "record");
+        assertEquals(expectedString.strip(), actual);
     }
 
     @Test
     public void recordInRecord() throws IOException {
-        String expectedString = "record ExampleRecord() {\n" +
-            "  record Example(\n" +
-            "      int id\n" +
-            "  ) {\n" +
-            "  }\n" +
-            "}\n";
+        String expectedString = """
+            package test;
+
+            public record ExampleRecord() {
+              record Example(
+                  int id
+              ) {
+              }
+            }""";
         RecordDef.RecordDefBuilder recordBuilder = RecordDef.builder("Example");
         PropertyDef.PropertyDefBuilder propertyBuilder = PropertyDef.builder("id").ofType(TypeDef.Primitive.INT);
         recordBuilder.addProperty(propertyBuilder.build());
 
         RecordDef.RecordDefBuilder classBuilder = getRecordDefBuilderWith(recordBuilder.build());
-        assertAndClean(generateClass(classBuilder.build()), expectedString);
+        String actual = writeClass(classBuilder.build(),"record");
+        assertEquals(expectedString.strip(), actual);
     }
 
     @Test
     public void classInRecord() throws IOException {
-        String expectedString = "record InnerRecord() {\n" +
-            "  class Inner {\n" +
-            "  }\n" +
-            "}\n";
+        String expectedString = """
+            package test;
+
+            public record InnerRecord() {
+              class Inner {
+              }
+            }""";
         ClassDef.ClassDefBuilder innerClassBuilder = ClassDef.builder("Inner");
 
         RecordDef.RecordDefBuilder classBuilder = getRecordDefBuilderWith(innerClassBuilder.build());
-        assertAndClean(generateClass(classBuilder.build()), expectedString);
+        String actual = writeClass(classBuilder.build(),"record");
+        assertEquals(expectedString.strip(), actual);
+    }
+
+    @Test
+    public void staticClassInRecord() throws IOException {
+        String expectedString = """
+            package test;
+
+            public record InnerRecord() {
+              private static class Inner {
+              }
+            }""";
+        ClassDef.ClassDefBuilder innerClassBuilder = ClassDef.builder("Inner");
+        innerClassBuilder.addModifiers(Modifier.STATIC, Modifier.PRIVATE);
+
+        RecordDef.RecordDefBuilder classBuilder = getRecordDefBuilderWith(innerClassBuilder.build());
+        String actual = writeClass(classBuilder.build(),"record");
+        assertEquals(expectedString.strip(), actual);
     }
 
     @Test
     public void interfaceInRecord() throws IOException {
-        String expectedString = "record InterfaceRecord() {\n" +
-            "  interface Interface {\n" +
-            "  }\n" +
-            "}\n";
+        String expectedString = """
+            package test;
+
+            public record InterfaceRecord() {
+              interface Interface {
+              }
+            }""";
         InterfaceDef.InterfaceDefBuilder interfaceBuilder = InterfaceDef.builder("Interface");
 
         RecordDef.RecordDefBuilder classBuilder = getRecordDefBuilderWith(interfaceBuilder.build());
-        assertAndClean(generateClass(classBuilder.build()), expectedString);
+        String actual = writeClass(classBuilder.build(),"record");
+        assertEquals(expectedString.strip(), actual);
     }
+
+    /** -----------------------------------------------------------
+     * INNER TYPES INSIDE AN INTERFACE
+     * -----------------------------------------------------------
+     */
 
     @Test
     public void enumInInterface() throws IOException {
-        String expectedString = "interface StatusInterface {\n" +
-            "  enum Status {\n" +
-            "\n" +
-            "    SINGLE,\n" +
-            "    MARRIED\n" +
-            "  }\n" +
-            "}\n";
+        String expectedString = """
+            package test;
+
+            public interface StatusInterface {
+              enum Status {
+
+                SINGLE,
+                MARRIED
+              }
+            }""";
         EnumDef.EnumDefBuilder enumBuilder = EnumDef.builder("Status");
         enumBuilder.addEnumConstant("SINGLE").addEnumConstant("MARRIED");
         EnumDef enumDef = enumBuilder.build();
 
         InterfaceDef.InterfaceDefBuilder classBuilder = getInterfaceDefBuilderWith(enumDef);
-        assertAndClean(generateClass(classBuilder.build()), expectedString);
+        String actual = writeClass(classBuilder.build(), "interface");
+        assertEquals(expectedString.strip(), actual);
     }
 
     @Test
     public void recordInInterface() throws IOException {
-        String expectedString = "interface RecordInterface {\n" +
-            "  record Record(\n" +
-            "      int id\n" +
-            "  ) {\n" +
-            "  }\n" +
-            "}\n";
+        String expectedString = """
+            package test;
+
+            public interface RecordInterface {
+              record Record(
+                  int id
+              ) {
+              }
+            }""";
         RecordDef.RecordDefBuilder recordBuilder = RecordDef.builder("Record");
         PropertyDef.PropertyDefBuilder propertyBuilder = PropertyDef.builder("id").ofType(TypeDef.Primitive.INT);
         recordBuilder.addProperty(propertyBuilder.build());
 
         InterfaceDef.InterfaceDefBuilder classBuilder = getInterfaceDefBuilderWith(recordBuilder.build());
-        assertAndClean(generateClass(classBuilder.build()), expectedString);
+        String actual = writeClass(classBuilder.build(),"interface");
+        assertEquals(expectedString.strip(), actual);
     }
 
     @Test
     public void classInInterface() throws IOException {
-        String expectedString = "interface InnerInterface {\n" +
-            "  class Inner {\n" +
-            "  }\n" +
-            "}\n";
+        String expectedString = """
+            package test;
+
+            public interface InnerInterface {
+              class Inner {
+              }
+            }""";
         ClassDef.ClassDefBuilder innerClassBuilder = ClassDef.builder("Inner");
 
         InterfaceDef.InterfaceDefBuilder classBuilder = getInterfaceDefBuilderWith(innerClassBuilder.build());
-        assertAndClean(generateClass(classBuilder.build()), expectedString);
+        String actual = writeClass(classBuilder.build(),"interface");
+        assertEquals(expectedString.strip(), actual);
     }
 
     @Test
     public void interfaceInInterface() throws IOException {
-        String expectedString = "interface InnerInterface {\n" +
-            "  interface Inner {\n" +
-            "  }\n" +
-            "}\n";
+        String expectedString = """
+            package test;
+
+            public interface InnerInterface {
+              interface Inner {
+              }
+            }""";
         InterfaceDef.InterfaceDefBuilder interfaceBuilder = InterfaceDef.builder("Inner");
 
         InterfaceDef.InterfaceDefBuilder classBuilder = getInterfaceDefBuilderWith(interfaceBuilder.build());
-        assertAndClean(generateClass(classBuilder.build()), expectedString);
+        String actual = writeClass(classBuilder.build(),"interface");
+        assertEquals(expectedString.strip(), actual);
     }
 
+    /** -----------------------------------------------------------
+     * HELPER METHODS
+     * -----------------------------------------------------------
+     */
+
     private static ClassDef.ClassDefBuilder getClassDefBuilderWith(ObjectDef objectDef) {
-        ClassDef.ClassDefBuilder classBuilder = ClassDef.builder(objectDef.getSimpleName() + "Class");
+        ClassDef.ClassDefBuilder classBuilder = ClassDef.builder("test." + objectDef.getSimpleName() + "Class")
+            .addModifiers(Modifier.PUBLIC);
         classBuilder.addInnerType(objectDef);
         return classBuilder;
     }
 
     private static RecordDef.RecordDefBuilder getRecordDefBuilderWith(ObjectDef objectDef) {
-        RecordDef.RecordDefBuilder classBuilder = RecordDef.builder(objectDef.getSimpleName() + "Record");
+        RecordDef.RecordDefBuilder classBuilder = RecordDef.builder("test." + objectDef.getSimpleName() + "Record")
+            .addModifiers(Modifier.PUBLIC);
         classBuilder.addInnerType(objectDef);
         return classBuilder;
     }
 
     private static InterfaceDef.InterfaceDefBuilder getInterfaceDefBuilderWith(ObjectDef objectDef) {
-        InterfaceDef.InterfaceDefBuilder classBuilder = InterfaceDef.builder(objectDef.getSimpleName() + "Interface");
+        InterfaceDef.InterfaceDefBuilder classBuilder = InterfaceDef.builder("test." + objectDef.getSimpleName() + "Interface")
+            .addModifiers(Modifier.PUBLIC);
         classBuilder.addInnerType(objectDef);
         return classBuilder;
-    }
-
-    private static File generateClass(ObjectDef objectDef) throws IOException {
-        try {
-            SourceGenerator sourceGenerator = SourceGenerators
-                .findByLanguage(VisitorContext.Language.JAVA).orElse(null);
-            if (sourceGenerator == null) {
-                return null;
-            }
-
-            File outputFile = new File("Example.java");
-
-            if (!outputFile.exists() && !outputFile.createNewFile()) {
-                throw new IOException("Could not create file " + outputFile.getAbsolutePath());
-            }
-            try (FileWriter writer = new FileWriter(outputFile)) {
-                sourceGenerator.write(objectDef, writer);
-            }
-            return outputFile;
-        } catch (ProcessingException | IOException e) {
-            throw e;
-        }
-    }
-
-    private static String compareFileContentWithString(File file, String comparisonString) {
-        try {
-            return new String(Files.readAllBytes(Path.of(file.toURI())));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "";
     }
 }
