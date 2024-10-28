c35a59147072ff88fc7874f44357243b666e090b
elifKurtay <elif.kurtay@oracle.com>
2024-10-28 17:42:50 +0100

    Annotated typedef with tests

--- sourcegen-generator-java/src/test/java/io/micronaut/sourcegen/javapoet/write/FieldDefTest.java
+++ sourcegen-generator-java/src/test/java/io/micronaut/sourcegen/javapoet/write/FieldDefTest.java
@@ -1,11 +1,19 @@
 package io.micronaut.sourcegen.javapoet.write;
 
+import io.micronaut.sourcegen.JavaPoetSourceGenerator;
+import io.micronaut.sourcegen.model.AnnotationDef;
+import io.micronaut.sourcegen.model.ClassTypeDef;
 import io.micronaut.sourcegen.model.FieldDef;
+import io.micronaut.sourcegen.model.PropertyDef;
+import io.micronaut.sourcegen.model.RecordDef;
 import io.micronaut.sourcegen.model.TypeDef;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class FieldDefTest extends AbstractWriteTest {
@@ -30,6 +38,30 @@ public class FieldDefTest extends AbstractWriteTest {
         );
 
         assertEquals("byte[][] byteArray;", result);
+    }
+
+
+    @Test public void annotatedGenericField() throws Exception {
+        PropertyDef.PropertyDefBuilder propertyDef = PropertyDef.builder("numbers");
+        var MIN_ANN = AnnotationDef.builder(ClassTypeDef.of("jakarta.validation.constraints.Min")).addMember("value", 1).build();
+        propertyDef.ofType(TypeDef.parameterized(ClassTypeDef.of(List.class), TypeDef.Primitive.FLOAT.wrapperType().annotated(MIN_ANN)));
+
+        RecordDef.RecordDefBuilder builder = RecordDef.builder("Record").addProperty(propertyDef.build());
+        JavaPoetSourceGenerator generator = new JavaPoetSourceGenerator();
+        String result;
+        try (StringWriter writer = new StringWriter()) {
+            generator.write(builder.build(), writer);
+            result = writer.toString();
+        }
+
+        assertThat(result).isEqualTo("import jakarta.validation.constraints.Min;\n" +
+            "import java.lang.Float;\n" +
+            "import java.util.List;\n" +
+            "\n" +
+            "record Record(\n" +
+            "    List<@Min(1) Float> numbers\n" +
+            ") {\n" +
+            "}\n");
     }
 
 }
