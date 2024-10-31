d19f781e97a7127434981b0229e9e6ffd5bc796a
Andriy Dmytruk <andriy.dmytruk@oracle.com>
2024-10-31 14:34:48 -0400

    Fix validation tests

--- test-suite-java/build.gradle.kts
+++ test-suite-java/build.gradle.kts
@@ -6,16 +6,15 @@ dependencies {
     annotationProcessor(mn.micronaut.inject.java)
     annotationProcessor(projects.sourcegenGeneratorJava)
     annotationProcessor(projects.testSuiteCustomGenerators)
+    annotationProcessor(mnValidation.micronaut.validation.processor)
 
     implementation(projects.sourcegenAnnotations)
     implementation(projects.testSuiteCustomAnnotations)
-    implementation(mnValidation.validation)
+    implementation(mnValidation.micronaut.validation)
+
     testImplementation(mnTest.micronaut.test.junit5)
     testImplementation(libs.junit.jupiter.engine)
-    testAnnotationProcessor(mn.micronaut.inject.java)
-
-    testAnnotationProcessor(mnValidation.micronaut.validation.processor)
-    testImplementation(mnValidation.micronaut.validation)
+    testAnnotationProcessor(mn.micronaut.inject.java.test)
 }
 //
 //tasks {
